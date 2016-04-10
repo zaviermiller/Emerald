4556575d4c343d0d3fa515857e50db03e0313401
zaviermiller <xavier2miller@gmail.com>
2016-04-10 04:15:46 -0500

    working on fixes

--- db/migrate/20160404180118_create_lessons.rb
+++ db/migrate/20160404180118_create_lessons.rb
@@ -1,9 +1,12 @@
 class CreateLessons < ActiveRecord::Migration
-  def change
+  def up
     create_table :lessons do |t|
       t.text :code
 
       t.timestamps null: false
+    end
+    def down 
+    	drop_table :lessons
     end
   end
 end
