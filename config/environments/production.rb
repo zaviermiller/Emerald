9dcb5799735f8e33ba5e96b5442814fa3f40701f
zaviermiller <xavier2miller@gmail.com>
2016-05-04 06:10:10 -0500

    mailer issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,11 +47,13 @@ Rails.application.configure do
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
-  config.action_mailer.default_url_options = { :host => 'codeeemerald.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = true
 config.action_mailer.default :charset => "utf-8"
+config.action_mailer.default_url_options = { host: 'codeemerald.herokuapp.com' }
+
+config.action_mailer.delivery_method = :smtp
+
 config.action_mailer.smtp_settings = {
 address: "smtp.gmail.com",
 port: 587,
